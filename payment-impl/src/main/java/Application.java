cb2074d10d98c6fa9dff60054a73712f072ad13e
misa130 <misasevervv@mail.ru>
2020-04-17 00:38:26 +0400

    Идет поиск ошибки невозможности найти класс Application

--- payment-impl/src/main/java/Application.java
+++ payment-impl/src/main/java/Application.java
@@ -1,9 +1,7 @@
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class Application {
-    //private static final Logger log = LoggerFactory.getLogger();
     public static void main(String[] args) {
-
+        log.info("Hello world");
     }
 }
