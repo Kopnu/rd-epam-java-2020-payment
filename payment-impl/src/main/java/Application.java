13128e91634a88cdbd1635f3bd1f4fd29c62fa31
Kopnu <Dzivv0@yandex.ru>
2020-04-17 00:40:46 +0400

    Refactor logging

--- payment-impl/src/main/java/Application.java
+++ payment-impl/src/main/java/Application.java
@@ -3,6 +3,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Application {
     public static void main(String[] args) {
-        log.trace("Hello home!");
+        log.error("Hello error home!");
+        log.warn("Hello warn home!");
+        log.info("Hello info home!");
+        log.debug("Hello debug home!");
+        log.trace("Hello trace home!");
     }
 }
