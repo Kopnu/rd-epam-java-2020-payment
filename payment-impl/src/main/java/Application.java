29b9f4d3e8c827695b54e3140f8418dc0b1d1d26
misa130 <misasevervv@mail.ru>
2020-04-17 00:09:15 +0400

    Идет поиск ошибки невозможности использования @Slf4j

--- payment-impl/src/main/java/Application.java
+++ payment-impl/src/main/java/Application.java
@@ -1,4 +1,8 @@
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
 public class Application {
+    //private static final Logger log = LoggerFactory.getLogger();
     public static void main(String[] args) {
 
     }
