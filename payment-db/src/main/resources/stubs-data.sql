6ff567dd3d400df321aa4504e307a0a7af354581
LopatinDmitrii <l.dmi3.v@gmail.com>
2020-04-21 14:01:41 +0400

    Add init for DB (#14)

--- payment-db/src/main/resources/stubs-data.sql
+++ payment-db/src/main/resources/stubs-data.sql
@@ -1,25 +1,25 @@
 insert into payment.pm_account_statuses (id, status) values
-('1','Активен'),
-('2','Закрыт');
+	('1','РђРєС‚РёРІРµРЅ'),
+	('2','Р—Р°РєСЂС‹С‚');
 
 insert into payment.pm_accounts (account_id, client_id, account_number, ogrn, kpp, inn, account_status_id) values
-('1','1','898989898912','12345678901238','098765432','123456789012','1'),
-('2','1','898989898923','12345678902349','087654321','123456789123','2');
+	('1','1','898989898912','12345678901238','098765432','123456789012','1'),
+	('2','1','898989898923','12345678902349','087654321','123456789123','2');
 
 insert into payment.pm_clients (client_id, name, description) values
-('1','Гоша', 'Описание Гоши'),
-('2','Бука','Описание Буки');
+	('1','Р“РѕС€Р°', 'РћРїРёСЃР°РЅРёРµ Р“РѕС€Рё'),
+	('2','РђРЅС‚РѕРЅ','РћРїРёСЃР°РЅРёРµ РђРЅС‚РѕРЅР°');
 
 insert into payment.pm_payments (payment_private_id, payment_public_id, client_id, reciever_account_id, sender_account_id,amount,key) values
-('1','1','1','2','1','10000','123445532'),
-('2','2','2','1','2','999999','243532');
+	('1','1','1','2','1','10000','123445532'),
+	('2','2','2','1','2','999999','243532');
 
 insert into payment.pm_accept_statuses (id, status) values
-('1','Принят в обработку'),
-('2','Выставлен'),
-('3','Оплачен'),
-('4','Ошибка');
+	('1','РџСЂРёРЅСЏС‚ РІ РѕР±СЂР°Р±РѕС‚РєСѓ'),
+	('2','Р’С‹СЃС‚Р°РІР»РµРЅ'),
+	('3','РћРїР»Р°С‡РµРЅ'),
+	('4','РћС€РёР±РєР°');
 
 insert into payment.pm_accepts (accept_id, payment_public_id, client_callback_url,client_payment_url,key,accept_status_id) values
-('1','1','firtsclient','secondclient','42523906','1'),
-('2','2','secondclient','firstclient','3575732','3');
+	('1','1','firtsclient','secondclient','42523906','1'),
+	('2','2','secondclient','firstclient','3575732','3');
