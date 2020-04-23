85580501fd6d734aa9cae908dbcdeeb654c5e72b
misa130 <misasevervv@mail.ru>
2020-04-23 22:40:48 +0400

    Добавлены скрипты для создания схемы с заполненными таблицами, пользователя

---
+++ payment-db/src/main/resources/init.sql
@@ -0,0 +1,61 @@
+drop schema if exists payment cascade;
+create schema payment;
+
+drop user if exists pay;
+create user pay password 'pay' valid until 'infinity';
+
+grant all on schema payment to pay;
+grant all privileges on all sequences in schema payment to pay;
+grant all privileges on all functions in schema payment to pay;
+
+create table payment.pm_account_statuses (
+	id varchar(64) primary key,
+	status varchar(14)
+);
+
+create table payment.pm_accept_statuses(
+	id varchar(64) primary key,
+	status varchar(36)
+);
+
+create table payment.pm_clients(
+	client_id varchar(64) primary key,
+	name varchar(40),
+	description text
+);
+
+create table payment.pm_accounts (
+	account_id varchar(64) primary key,
+	client_id varchar(64),
+	account_number varchar(24),
+	ogrn varchar(26),
+	kpp varchar(18),
+	inn varchar(24),
+	account_status_id varchar(64),
+	foreign key (account_status_id) references payment.pm_account_statuses(id),
+	foreign key (client_id) references payment.pm_clients(client_id)
+);
+
+create table payment.pm_payments (
+	payment_private_id varchar(64) primary key,
+	payment_public_id varchar(64),
+	client_id varchar(64),
+	reciever_account_id varchar(64),
+	sender_account_id varchar(64),
+	amount numeric,
+	key varchar(64),
+	foreign key (client_id) references payment.pm_clients(client_id),
+	foreign key (reciever_account_id) references payment.pm_accounts(account_id),
+	foreign key (sender_account_id) references payment.pm_accounts(account_id)
+);
+
+create table payment.pm_accepts (
+	accept_id varchar(64) primary key,
+	payment_private_id varchar(64),
+	client_callback_url varchar(400),
+	client_payment_url varchar(400),
+	key varchar(64),
+	accept_status_id varchar(64),
+	foreign key (payment_private_id) references payment.pm_payments(payment_private_id),
+	foreign key (accept_status_id) references payment.pm_accept_statuses(id)
+);
---
+++ payment-db/src/main/resources/stub_insert.sql
@@ -0,0 +1,25 @@
+insert into payment.pm_account_statuses (id, status) values
+	('1','Активен'),
+	('2','Закрыт');
+
+insert into payment.pm_accept_statuses (id, status) values
+	('1','Принят в обработку'),
+	('2','Выставлен'),
+	('3','Оплачен'),
+	('4','Ошибка');
+
+insert into payment.pm_clients (id, status) values
+	('1', 'Dzimitry', 'самобытно'),
+	('2', 'Вася', 'не самобытно')
+
+insert into payment.pm_accounts (account_id, client_id, account_number, ogrn, kpp, inn, account_status_id) values
+	('1','1', '89188114787', '12345677654321', '897564231', '123456654321', '1'),
+	('2','1', '89748775487', '76543211234567', '123456789', '654321123456', '2');
+
+insert into payment.pm_payments (payment_private_id, payment_public_id, client_id, reciever_account_id, sender_account_id,amount,key) values
+	('1','1','1','2','1','100500','87778887777'),
+	('2','2','2','1','2','1','54878548');
+
+insert into payment.pm_accepts (accept_id, payment_private_id, client_callback_url,client_payment_url,key,accept_status_id) values
+	('1','1','qwertyqwerty','ytrewqytrewq','33443321','1'),
+	('2','2','ytrewqytrewq','qwertyqwerty','35232333','3');
